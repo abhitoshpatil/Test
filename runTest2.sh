bc404d388cda0c7f21aaae25dc9290555bb3b69d
Abhitosh Patil <abhitosh.patil@zs.com>
2021-09-22 07:41:36 +0000

    Added shell

---
+++ runTest2.sh
@@ -0,0 +1,20 @@
+scriptName=Test.jmx
+time=$(date "+%Y.%m.%d-%H.%M")
+#cd ./${platform}/
+results=PerfTest_Results_$time
+mkdir ./$results
+mkdir ./HTMLReport_$time
+nohup sh apache-jmeter-5.5-SNAPSHOT/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=csv -n -t ./$scriptName -f -l ./Test.jtl -e -o ./HTMLReport_$time > ./$results/nohup.txt 2>&1
+sh apache-jmeter-5.5-SNAPSHOT/bin/JMeterPluginsCMD.sh --generate-csv ./Temp_$time.csv --input-jtl ./Test.jtl --plugin-type AggregateReport
+cut -d',' -f1,2,3,5,8,9,10 Temp_$time.csv > Temp2_$time.csv
+head -n1 Temp2_$time.csv && tail -n+2 Temp2_$time.csv | sort >> Temp3_$time.csv
+awk -F: '/^\\[TC]/{print $1}' Temp3_$time.csv >> Aggregate_$time.csv
+sed -i '1s/.*/ Label,Samples#,Average,90%,Min,Max,Error%/' Aggregate_$time.csv
+mv ./HTMLReport_$time ./$results
+mv ./Test.jtl ./$results
+mv ./jmeter.log ./$results
+#mv ./GrafanaResults.txt ./$results
+mv ./Aggregate_$time.csv ./$results
+mv ./Test.properties ./$results
+rm ./Temp*.csv
+echo "Test Finished. Find test results in folder $results"
