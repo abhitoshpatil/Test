96c09424a9a1a3f71f903ae3276201fa168c645d
Abhitosh Patil <abhitosh.patil@zs.com>
2021-09-22 11:35:38 +0000

    Shell

--- runTest2.sh
+++ runTest2.sh
@@ -4,14 +4,14 @@ time=$(date "+%Y.%m.%d-%H.%M")
 results=PerfTest_Results_$time
 mkdir ./$results
 mkdir ./HTMLReport_$time
-nohup sh apache-jmeter-5.5-SNAPSHOT/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=csv -n -t ./$scriptName -f -l ./Test$time.jtl -e -o ./HTMLReport_$time > ./$results/nohup.txt 2>&1
-sh apache-jmeter-5.5-SNAPSHOT/bin/JMeterPluginsCMD.sh --generate-csv ./Aggregate_$time.csv --input-jtl ./Test$time.jtl --plugin-type AggregateReport
+nohup sh apache-jmeter-5.5-SNAPSHOT/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=csv -n -t ./$scriptName -f -l ./Test_$time.jtl -e -o ./HTMLReport_$time > ./$results/nohup.txt 2>&1
+sh apache-jmeter-5.5-SNAPSHOT/bin/JMeterPluginsCMD.sh --generate-csv ./Aggregate_$time.csv --input-jtl ./Test_$time.jtl --plugin-type AggregateReport
 #cut -d',' -f1,2,3,5,8,9,10 Temp_$time.csv > Temp2_$time.csv
 #head -n1 Temp2_$time.csv && tail -n+2 Temp2_$time.csv | sort >> Temp3_$time.csv
 #awk -F: '/^\\[TC]/{print $1}' Temp3_$time.csv >> Aggregate_$time.csv
 #sed -i '1s/.*/ Label,Samples#,Average,90%,Min,Max,Error%/' Aggregate_$time.csv
 mv ./HTMLReport_$time ./$results
-mv ./Test$time.jtl ./$results
+mv ./Test_$time.jtl ./$results
 mv ./jmeter.log ./$results
 #mv ./GrafanaResults.txt ./$results
 mv ./Aggregate_$time.csv ./$results
